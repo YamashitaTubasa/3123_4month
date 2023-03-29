768406c9d3249272d299482146310bf7e511d410
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2023-03-29 11:19:27 +0900

    一応できた

--- DirectXGame/Engin/3d/RailCamera.cpp
+++ DirectXGame/Engin/3d/RailCamera.cpp
@@ -18,7 +18,7 @@ void RailCamera::Initialize(Object3d* player_) {
 	camera->SetPosition(viewProjection->eye);
 	camera->SetRotation(viewProjection->target);
 	player_->SetParent3d(camera);
-	player_->SetScale(Vector3(3, 3, 3));
+	player_->SetScale(Vector3(0.4, 0.4, 0.4));
 	player_->SetRotation(Vector3(0, 90, 0));
 	player_->SetPosition(Vector3(0, 0, 2.5));
 	velo = 0.8f;
@@ -37,15 +37,16 @@ void RailCamera::Update(Object3d* player_, std::vector<Vector3>& point) {
 	camera_ = spline_.Update(point, timeRate);
 
 	camera->SetPosition(camera_ + frontTmp * 0.5);
-	camera->SetRotationY(std::atan2(frontTmp.z, frontTmp.x) + frontTmp.x * 70);
+	player_->SetPosition(camera->GetPosition()+ frontTmp);
+	player_->SetRotationY(std::atan2(frontTmp.z, frontTmp.x) + frontTmp.x * 70 + 90);
 	Vector3 rotaVec = { frontTmp.x,0,frontTmp.z };
 	rotaVec.length();
-	camera->SetRotationX(std::atan2(-frontTmp.y, rotaVec.z) + frontTmp.y * 70);
+	player_->SetRotationX(std::atan2(-frontTmp.y, rotaVec.z) + frontTmp.y * 70);
 	Vector3 answer = camera->GetRotation();
 
 	//XV
 	camera->Update();
-	viewProjection->target = ( camera->GetPosition() );
+	viewProjection->target = (camera_ + frontTmp * 0.5);
 	viewProjection->eye = ( camera_ - frontTmp );
 	viewProjection->UpdateMatrix();
 
