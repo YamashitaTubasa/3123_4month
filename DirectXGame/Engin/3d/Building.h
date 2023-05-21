da0a597e020be0865702c2e36e6ad4a11051e59a
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-05-22 01:19:00 +0900

    背景オブジェクトを増やした

--- DirectXGame/Engin/3d/Building.h
+++ DirectXGame/Engin/3d/Building.h
@@ -19,10 +19,12 @@ private:
 	Model* builModel01;
 	Model* builModel02;
 	Model* builModel03;
+	Model* ringModel;
 
 	Object3d* buil_01[5];
 	Object3d* buil_02[5];
 	Object3d* buil_03[5];
+	Object3d* ring[5];
 
 	RailCamera* railCamera;
 };
