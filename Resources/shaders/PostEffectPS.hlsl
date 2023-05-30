405847b829ef733d576a90d86e3363cb870440ad
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-05-30 12:49:32 +0900

    PostEffectPSの変更

--- Resources/shaders/PostEffectPS.hlsl
+++ Resources/shaders/PostEffectPS.hlsl
@@ -5,35 +5,57 @@ SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    // UV指定したピクセルの色をサンプリング
-    float4 texcolor = tex.Sample(smp, input.uv) * color;
-    float luminance = 1.0f; // 輝度
-    float setU, setV;
+    
+    //// UV指定したピクセルの色をサンプリング
+    //float4 texcolor = tex.Sample(smp, input.uv) * color;
+    //float luminance = 1.0f; // 輝度
+    //float setU, setV;
+    //float4 col = float4(0, 0, 0, 1);
+    ////========================= ブラー =========================//
+    //    float blur = 4.0;
+    //    luminance = 1.0f;
+    //    if (isBlur == false) {
+    //        if (blur >= 0.0f) {
+    //            blur--;
+    //        }
+    //    }
+    //    setU = blur / 1280.0f;
+    //    setV = blur / 720.0f;
+        
+    //    float shift = 0.005;
 
-    //========================= ブラー =========================//
-    if (isBlur == true ) {
-        float blur = 4.0;
-        luminance = 1.0f;
-        if (isBlur == false) {
-            if (blur >= 0.0f) {
-                blur--;
+    //    col += tex.Sample(smp, input.uv + float2(-shift, -shift));
+    //    col += tex.Sample(smp, input.uv + float2(0.0f, -shift));
+    //    col += tex.Sample(smp, input.uv + float2(shift, -shift));
+    //    col += tex.Sample(smp, input.uv + float2(-shift, 0.0f));
+    //    col += tex.Sample(smp, input.uv + float2(0.0f, 0.0f));
+    //    col += tex.Sample(smp, input.uv + float2(shift, 0.0f));
+    //    col += tex.Sample(smp, input.uv + float2(-shift, shift));
+    //    col += tex.Sample(smp, input.uv + float2(0.0f, shift));
+    //    col += tex.Sample(smp, input.uv + float2(shift, shift));
+        
+    //    col.rgb /= 9.0f;
+    //    col.a = 1;
+    
+    float luminance = 1.0f; // 輝度
+    float shiftWidth = 0.003;
+    float shiftNum = 5;
+
+    float4 col = tex.Sample(smp, input.uv) * color;
+    float num = 0;
+
+    if (isBlur == true)
+    {
+        for (float py = -shiftNum / 2; py <= shiftNum / 2; py++)
+        {
+            for (float px = -shiftNum / 2; px <= shiftNum / 2; px++)
+            {
+                col += tex.Sample(smp, input.uv + float2(px, py) * shiftWidth) * color;
+                num++;
             }
         }
-        setU = blur / 1280.0f;
-        setV = blur / 720.0f;
-
-        texcolor += tex.Sample(smp, input.uv + float2(setU, 0.0f));
-        texcolor += tex.Sample(smp, input.uv + float2(-setU, 0.0f));
-        texcolor += tex.Sample(smp, input.uv + float2(0.0f, -setV));
-        texcolor += tex.Sample(smp, input.uv + float2(0.0f, setV));
-        texcolor += tex.Sample(smp, input.uv + float2(setU, setV));
-        texcolor += tex.Sample(smp, input.uv + float2(setU, -setV));
-        texcolor += tex.Sample(smp, input.uv + float2(-setU, setV));
-        texcolor += tex.Sample(smp, input.uv + float2(-setU, -setV));
-
-        texcolor /= 10.0f;
+        col.rgb /= num;
     }
-
-	// アルファに1を入れて出力
-	return float4(texcolor.rgb * luminance, alpha);
+    
+    return float4(col.rgb * luminance, alpha);
 }
